e93a1d3d622bb638e057c2a38c108bacfa852d03
anker-afk <attamsaina@gmail.com>
2026-08-27 17:23:10 -0500

    week 1 functions

--- mini-repo/core/util/peripherals/imu/BNO055.cpp
+++ mini-repo/core/util/peripherals/imu/BNO055.cpp
@@ -1,4 +1,108 @@
 #include "BNO055.h"
+#include "mbed.h"
+
+BNO055::BNO055(I2C &i2c, uint8_t addr, PinName p_reset) noexcept
+    : IMU(), _i2c(i2c), chip_addr(addr), _res(p_reset), cantReadDataCount(0)
+{
+    _i2c.frequency(100000);
+
+    _res.write(0);
+    ThisThread::sleep_for(10ms);
+    _res.write(1);
+
+    ThisThread::sleep_for(650ms);
+
+}
+void BNO055 :: init() noexcept {
+
+    char config_mode[] ={0x3D, 0x00};
+    _i2c.write(chip_addr << 1, config_mode, 2, false);
+    ThisThread :: sleep_for(19ms);
+
+    char dof_mode[]  = {0x3D, 0x0C};
+    _i2c.write(chip_addr << 1, dof_mode, 2, false);
+    ThisThread :: sleep_for(9ms);
+
+}
+void BNO055 :: reset() noexcept{
+    _res.write(0);
+    ThisThread :: sleep_for(10ms);
+
+    _res.write(1);
+    ThisThread :: sleep_for(650ms);
+
+    init();
+}
+void BNO055 :: get_accel(BNO055_VECTOR_TypeDef *la) {
+    if (cantReadDataCount > 0 && cantReadDataCount < 50) {
+        cantReadDataCount++;
+        return;
+    } else if (cantReadDataCount >= 50) {
+        cantReadDataCount = 1;
+    }
+
+    char accel[] = {0x08};
+    int16_t x,y,z;
+    int writeResult = _i2c.write(chip_addr << 1, accel, 1, true);
+
+    if (!writeResult)  {
+        if (cantReadDataCount > 0) {
+            printf("RESET IMU\n");
+            reset();
+            cantReadDataCount = 0;
+        }
+    
+        _i2c.read(chip_addr << 1 | 1 , dt, 6, false);
+        x = dt[1] << 8 | dt[0];
+        y = dt[3] << 8 | dt[2];
+        z = dt[5] << 8 | dt[4];
+
+        la->x = (double)x / 100;
+        la->y = (double)y / 100;
+        la->z = (double)z / 100;
+    }
+    else{
+        cantReadDataCount ++;
+    }
+ 
+}
+//The gyro function I didnt copy the cant read data count from get quarternion
+void BNO055 :: get_gyro(BNO055_VECTOR_TypeDef *gr){
+    int16_t x,y,z;
+    char gyro[] = {0x14};
+
+    _i2c.write(chip_addr << 1, gyro, 1, true);
+    _i2c.read(chip_addr << 1 | 1, dt, 6, false);
+
+    x = dt[1] << 8 | dt[0];
+    y = dt[3] << 8 | dt[2];
+    z = dt[5] << 8 | dt[4];
+
+    gr -> x = (double)x / 16.0;
+    gr -> y = (double)y / 16.0;
+    gr -> z = (double)z / 16.0;
+}
+//read override
+IMU :: EulerAngles BNO055 ::  read() {
+    get_angular_position_quat(&imuAngles);
+    return imuAngles;
+
+}
+IMU :: EulerAngles BNO055 :: getImuAngles(){
+    return imuAngles;
+}
+//change fusion mode
+void BNO055 :: change_fusion_mode(uint8_t mode){
+    //first change to config mode
+    char change[] = {0x3D, 0X00};
+    _i2c.write(chip_addr << 1, change, 2, false);
+    ThisThread :: sleep_for(19ms);
+
+    //then change to the mode
+    char change_to_mode[] = {0x3D, (char)mode};
+    _i2c.write(chip_addr << 1, change_to_mode, 2, false);
+    ThisThread :: sleep_for(9ms);
+}
 
 
 /*
